4864e2b346dfe574ec3672eeb09cfcace3cfe265
DiegoSalado <diegoabel.salado@gmail.com>
2022-02-14 07:58:45 +0000

    mas limpios los datos

--- archivos/script.sh
+++ archivos/script.sh
@@ -5,7 +5,8 @@ echo "Descomprimiendo datos..."
 unzip datos_abiertos_covid19.zip
 echo "Para obtener la información de los casos positivos del estado de Zacatecas presione ENTER"
 read holder
-csvgrep -c ENTIDAD_RES -m 32 220212COVID19MEXICO.csv | csvgrep -c 36 -r "([1-3])" > positivosZac.csv
-rm 220212COVID19MEXICO.csv
+csvgrep -c ENTIDAD_RES -m 32 220212COVID19MEXICO.csv | csvgrep -c 36 -r "([1-3])" > positivos.csv
+csvcut -C 1,2,3,7,17,19,20,31,32,33,34,35,37,38,39 positivos.csv > positivosZac.csv
+rm 220212COVID19MEXICO.csv positivos.csv
 echo "Listo! Los datos se encuantran en el archivo:"
 ls positivosZac.csv
